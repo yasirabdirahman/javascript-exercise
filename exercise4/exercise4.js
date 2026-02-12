052c6c3a4508dae39d4e8c383cd3076b8fd6d746
yasirabdirahman <essaaptherahman@gmail.com>
2026-02-11 21:35:40 -1000

    submit exercise

--- exercise4/exercise4.js
+++ exercise4/exercise4.js
@@ -1,7 +1,20 @@
-function add(x, y) {
-    console.log( "the sum of " + x + " and " + y + " is: " + (x + y));
+// q1
+function sum(x, y) {
+    console.log("sum of x+y is " + (x + y));
 
 }
+sum(5, 10);
+sum(20, 30);
+sum(100, 200);
+sum(1, 2);
+sum(0, 0); 
+
+// q2
+const add = function(x,y) {
+    console.log("result is " + (x + y));
+};
+
+add(3,4); 
 add(5, 10);
 add(20, 30);
 add(100, 200);
