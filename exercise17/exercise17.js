3eaeb382d97722de51e517ded976b9f950ebc9ae
yasirabdirahman <essaaptherahman@gmail.com>
2026-03-11 06:07:41 -0900

    submit exercise

---
+++ exercise17/exercise17.js
@@ -0,0 +1,11 @@
+let tempreture = 19;
+if (tempreture <= 0 ) {
+    console.log("very cold")
+} else if (tempreture <= 15){
+    console.log("cold")
+} else if (tempreture <=25){
+    console.log("warm")
+}
+ else 
+{ console.log("hot")
+}
