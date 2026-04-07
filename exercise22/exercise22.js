598b016076176433eb44c223857a1bb1f93f17bc
yasirabdirahman <essaaptherahman@gmail.com>
2026-04-07 11:40:26 -0900

    submit exercise

---
+++ exercise22/exercise22.js
@@ -0,0 +1,10 @@
+// const name = "ciise";
+// const university = "amoud university";
+// const massage = `hello, ${name} welcome to ${university}`;
+
+// console.log(massage);
+
+const color = ["red", "green", "blue" ];
+let = [one, two, three] = color ;
+console.log(one);
+console.log(three);
