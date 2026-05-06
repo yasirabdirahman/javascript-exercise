9f139d010e900d5567ba87476635a96b6367bedc
yasirabdirahman <essaaptherahman@gmail.com>
2026-05-06 09:26:22 +0300

    submit exercise

---
+++ exercise32/exercise32.js
@@ -0,0 +1,10 @@
+
+const portfolioTitle = document.querySelector('#ciwanka');
+const aboutText = document.querySelector('.description');
+
+console.log("Main Title Element:", portfolioTitle);
+console.log("Description Text:", aboutText);
+
+const allProjects = document.querySelectorAll('.item');
+
+console.log("Dhammaan Mashaariicda:", allProjects);
