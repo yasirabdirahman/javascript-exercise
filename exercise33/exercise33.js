39854c2bc3e9c3b8df9925d117682ebd349e4bdb
yasirabdirahman <essaaptherahman@gmail.com>
2026-05-06 22:51:50 +0300

    submit exercise

---
+++ exercise33/exercise33.js
@@ -0,0 +1,9 @@
+function changeContent() {
+            const ciwanka = document.querySelector("#ciwanka");
+            const paragraph = document.querySelector("#paragraph");
+
+            
+            ciwanka.textContent = "kuso dhawaw portofolio hayga";
+          
+            paragraph.innerHTML = "bage uu isbadalay ";
+        }
